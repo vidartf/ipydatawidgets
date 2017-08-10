adc65b5b2bf3c0b833eccb487838a5906a97f71f
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-08-10 17:56:42 +0200

    Adjust for distribution

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -6,3 +6,11 @@ export {
 export {
   JSONToUnion, unionToJSON, data_union_serialization
 } from './union';
+
+export {
+  JUPYTER_DATAWIDGETS_VERSION
+} from './version';
+
+export
+const version = (require('../package.json') as any).version;
+
---
+++ ts/src/version.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+/**
+ * The version of the Jupyter data widgets attribute spec that this package
+ * implements.
+ */
+export
+const JUPYTER_DATAWIDGETS_VERSION = '1.0.0';
