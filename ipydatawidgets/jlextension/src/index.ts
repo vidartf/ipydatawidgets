6654bc78ff26403e2194049efe3869bc54f26065
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-08-14 15:17:56 +0200

    Refactor jlab extension

--- ipydatawidgets/jlextension/src/index.ts
+++ ipydatawidgets/jlextension/src/index.ts
@@ -1,10 +1,59 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import NBDiffProvider from './plugin';
+import {
+  JupyterLabPlugin, JupyterLab
+} from '@jupyterlab/application';
 
-export default NBDiffProvider;
+import {
+  Token
+} from '@phosphor/coreutils';
 
-export {
-  IDataWidgetsExtension
-} from './plugin';
+import * as dataWidgets from 'jupyter-datawidgets';
+
+import {
+  INBWidgetExtension
+ } from "@jupyter-widgets/jupyterlab-manager";
+
+
+const EXTENSION_ID = 'jupyter.extensions.datawidgets'
+
+
+/**
+ * The token identifying the JupyterLab plugin.
+ */
+export
+const IDataWidgetsExtension = new Token<IDataWidgetsExtension>(EXTENSION_ID);
+
+/**
+ * The type of the provided value of the plugin in JupyterLab.
+ */
+export
+interface IDataWidgetsExtension {
+};
+
+
+/**
+ * The notebook diff provider.
+ */
+const dataWidgetsProvider: JupyterLabPlugin<IDataWidgetsExtension> = {
+  id: EXTENSION_ID,
+  requires: [INBWidgetExtension],
+  activate: activateWidgetExtension,
+  autoStart: true
+};
+
+export default dataWidgetsProvider;
+
+
+/**
+ * Activate the widget extension.
+ */
+function activateWidgetExtension(app: JupyterLab, widgetsManager: INBWidgetExtension): IDataWidgetsExtension {
+  widgetsManager.registerWidget({
+      name: 'jupyter-datawidgets',
+      version: dataWidgets.JUPYTER_DATAWIDGETS_VERSION,
+      exports: dataWidgets
+    });
+  return {};
+}
