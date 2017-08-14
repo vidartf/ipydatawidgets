691807b1d2dfabcc5dbb7ffda3027be370c3d132
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-08-14 16:15:03 +0200

    Add header to test files

--- ipydatawidgets/tests/test_ndarray_trait.py
+++ ipydatawidgets/tests/test_ndarray_trait.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
 import pytest
 
 import numpy as np
