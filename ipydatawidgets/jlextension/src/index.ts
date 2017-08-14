97b3ab271913ae8d6e0d315cd860ca76c07fba25
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-08-14 15:05:25 +0200

    Add jupyterlab extension

---
+++ ipydatawidgets/jlextension/src/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import NBDiffProvider from './plugin';
+
+export default NBDiffProvider;
+
+export {
+  IDataWidgetsExtension
+} from './plugin';
