6600344b8666b8de64f11ffc5914489b803b57f4
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-03-05 13:30:27 +0100

    Update web deps

--- packages/jupyter-datawidgets/webpack.config.js
+++ packages/jupyter-datawidgets/webpack.config.js
@@ -1,6 +1,5 @@
-var loaders = [
+var rules = [
   { test: /\.ts$/, loader: 'ts-loader' },
-  { test: /\.json$/, loader: 'json-loader' },
   { test: /\.js$/, loader: "source-map-loader" },
 ];
 
@@ -12,7 +11,7 @@ module.exports = {
     libraryTarget: 'amd'
   },
   module: {
-    loaders: loaders
+    rules: rules
   },
   devtool: 'source-map',
   externals: ['@jupyter-widgets/base', 'jupyter-scales'],
