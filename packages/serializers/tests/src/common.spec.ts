04ce9441ebf960045eb5e054ab660c56a36e478b
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-10-15 15:06:43 +0200

    Tests for write-back

--- packages/serializers/tests/src/common.spec.ts
+++ packages/serializers/tests/src/common.spec.ts
@@ -1,7 +1,15 @@
 
 import {
-  getArray, IDataSource
+  WidgetModel
+} from '@jupyter-widgets/base';
+
+import {
+  getArray, IDataSource, setArray, IDataWriteBack, DataWidget
 } from '../../src';
+
+import {
+  createTestModel
+} from './util';
 
 import ndarray = require('ndarray');
 
@@ -11,11 +19,25 @@ class DummyDataSource implements IDataSource {
     this.array = ndarray(this.raw_data, [2, 3]);
   }
 
-  getNDArray(key?: string): ndarray {
+  getNDArray(key?: string): ndarray | null {
     return this.array;
   }
 
-  array: ndarray;
+  array: ndarray | null;
+}
+
+
+class DummyWriteBack extends DummyDataSource implements IDataWriteBack {
+
+  canWriteBack(key?: string): boolean {
+    return this.canWrite;
+  }
+
+  setNDArray(array: ndarray | null, key?: string): void {
+    this.array = array;
+  }
+
+  canWrite = true;
 }
 
 function createDataSource(): DummyDataSource {
@@ -38,6 +60,88 @@ describe('getArray', () => {
       expect(output!.data).to.be(src.raw_data);
       expect(output!.dtype).to.be('float32');
       expect(output!.shape).to.eql([2, 3]);
+  });
+
+});
+
+
+describe('setArray', () => {
+
+  const attrKey = 'myTestKey';
+  const userData = {id: 'myCustomData'};
+
+  it('should replace the array when current is null', () => {
+
+      // First set up a widget model with initial state
+      let model = createTestModel(WidgetModel, {
+        [attrKey]: null
+      });
+
+      let array = ndarray(new Float32Array([1, 2, 3, 4, 5, 10]));
+      setArray(model, attrKey, array, userData);
+      expect(model.get(attrKey)).to.be(array);
+
+  });
+
+  it('should replace the array when current is array', () => {
+
+    // First set up a widget model with initial state
+    let model = createTestModel(WidgetModel, {
+      [attrKey]: new Float32Array([4, 8, 6])
+    });
+
+    let array = ndarray(new Float32Array([1, 2, 3, 4, 5, 10]));
+    setArray(model, attrKey, array, userData);
+    expect(model.get(attrKey)).to.be(array);
+
+  });
+
+  it('should replace the array when current does not implement write-back', () => {
+
+    // First set up a widget model with initial state
+    let model = createTestModel(WidgetModel, {
+      [attrKey]: new DummyDataSource(new Float32Array([]))
+    });
+
+    let array = ndarray(new Float32Array([1, 2, 3, 4, 5, 10]));
+    setArray(model, attrKey, array, userData);
+    expect(model.get(attrKey)).to.be(array);
+
+  });
+
+  it('should replace the array when canWriteBack returns false', () => {
+
+    // First set up a widget model with initial state
+    const current = new DummyWriteBack(new Float32Array([]));
+    current.canWrite = false;
+
+    let model = createTestModel(WidgetModel, {
+      [attrKey]: current
+    });
+
+    let array = ndarray(new Float32Array([1, 2, 3, 4, 5, 10]));
+    setArray(model, attrKey, array, userData);
+    expect(model.get(attrKey)).to.be(array);
+    // Check no changes to current data:
+    expect(current.array!.data === current.raw_data);
+
+  });
+
+  it('should write back when current can write back', () => {
+
+    // First set up a widget model with initial state
+    const current = new DummyWriteBack(new Float32Array([]));
+    current.canWrite = true;
+
+    let model = createTestModel(WidgetModel, {
+      [attrKey]: current
+    });
+
+    let array = ndarray(new Float32Array([1, 2, 3, 4, 5, 10]));
+    setArray(model, attrKey, array, userData);
+    expect(model.get(attrKey)).to.be(current);
+    expect(current.array === array);
+
   });
 
 });
